de11b7b2822150ad7c618521a119aadf1a056764
Macys1704 <cherin1704@gmail.com>
2024-07-23 21:58:38 -0300

    react configurado

---
+++ src/login/index.js
@@ -0,0 +1,14 @@
+import logo from '../logo.svg';
+import '../login/index.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>oi</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
